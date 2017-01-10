71188a15cd94d717a8b36b01cbfc710ec0ce4c2a
mneumayer <matthew.neumayer@gmail.com>
2017-01-09 21:07:23 -0600

    Add files via upload

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -166,24 +166,27 @@ it(' menu changes visibility when the menu icon is clicked',function(){
 
 
 
-it('menu slides out and becomes visible and also slides back in and becmes hidden',function(){
+it('menu slides out and becomes visible and also slides back in and becomes hidden',function(){
+
+
+$('.icon-list').click(function(){
+  var slideMenu = document.body.className;
+
+expect(slideMenu).toContain(menu-hidden);
+});
 
 
 
-  var spyEvent = spyOnEvent('.icon-list', 'click');
-  $('.icon-list').click();
-  expect('click').toHaveBeenTriggeredOn('.icon-list');
-  expect(spyEvent).toHaveBeenTriggered();
-  $('.icon-list').click();
+
+});
+
+
+});
+/*  $('.icon-list').click();
   spyEvent.reset();
   expect('click').not.toHaveBeenTriggeredOn('.icon-list');
   expect(spyEvent).not.toHaveBeenTriggered();
-
-});
-
-
-});
-
+*/
 
 
 
